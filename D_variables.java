7f9596c4866a726009ed07ffe688131ea0a6823d
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-06 22:41:54 +0530

    primitive datatypes

--- D_variables.java
+++ D_variables.java
@@ -1,12 +1,26 @@
 public class D_variables {
     public static void main(String[] args) {
+        //primitive data types
         int a=10;
         int b=5;
         System.out.println(a);
         System.out.println(b);
+        byte c =10;
+        System.out.println(c);
+        short d = 20;
+        System.out.println(d);
+        long aa= 1234456;
+        System.out.println(aa);
+        float price = 10 ;
+        System.out.println(price);
+        boolean type=false;
+        System.out.println(type);
+        double dd=10.5;
+        System.out.println(dd);
+
+        a=5;
+        b=8;
         System.out.println(a+b);
-        String name ="rohit";
-        System.out.println(name);
 
     }
 }
