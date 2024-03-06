1fb803efd56b035cddf8d306f344cfaf4d5217b7
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-06 22:46:52 +0530

    comments

---
+++ E_comments.java
@@ -0,0 +1,10 @@
+public class E_comments {
+    public static void main(String[] args) {
+        //single line comment
+        /* multiple line code
+         first line
+         second line
+         */
+        System.out.println("comments in java...");
+    }
+}
