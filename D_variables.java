f3ec093b717c4990ad20b809f24fbe4ace832418
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-06 22:19:12 +0530

    variable

---
+++ D_variables.java
@@ -0,0 +1,12 @@
+public class D_variables {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+        String name ="rohit";
+        System.out.println(name);
+
+    }
+}
