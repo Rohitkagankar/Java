1f42fb6424bc1f0e9098077dcb813b57a57adb3d
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-07 18:54:51 +0530

    type casting

---
+++ J_typeCasting.java
@@ -0,0 +1,11 @@
+public class J_typeCasting {
+    public static void main(String[] args) {
+        float a =10.5f;
+        int b = (int)a;
+        System.out.println(b);
+
+        char ch ='a';
+        int number = ch;
+        System.out.println(number);
+    }
+}
