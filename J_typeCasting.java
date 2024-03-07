55ff5f2c275d1cde55bffe942c6123e8419d6036
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-07 18:58:18 +0530

    type casting a

--- J_typeCasting.java
+++ J_typeCasting.java
@@ -5,7 +5,10 @@ public class J_typeCasting {
         System.out.println(b);
 
         char ch ='a';
+        char ch2 ='b';
         int number = ch;
+        int num = ch2;
         System.out.println(number);
+        System.out.println(num);
     }
 }
