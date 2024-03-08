001cc04bcd3652abafe0b7994c836d8bbb9fd097
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-08 23:16:22 +0530

    loops

---
+++ M_Loops.java
@@ -0,0 +1,9 @@
+public class M_Loops {
+    public static void main(String[] args) {
+    int count=0;
+    while(count<10){
+        System.out.println("hello world "+ (count+1));
+        count++;
+    }
+    }
+}
