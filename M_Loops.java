191e9475611e129410ad4e0b0203236cca2f23de
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-09 19:03:40 +0530

    while loop

--- M_Loops.java
+++ M_Loops.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class M_Loops {
     public static void main(String[] args) {
     int count=0;
@@ -5,5 +7,26 @@ public class M_Loops {
         System.out.println("hello world "+ (count+1));
         count++;
     }
+
+    //print nos from 1 to 10
+    int counter = 1;
+    while (counter <=10) {
+        System.out.println("counter: "+ counter);
+        counter++;
+        
+    }
+
+    System.out.println("Enter a number: ");
+    Scanner sc = new Scanner(System.in);
+    int num =sc.nextInt();
+    counter= 1;
+    int sum=0;
+    while (counter<=num) {
+        System.out.println(counter);
+        sum=sum+counter;
+        counter++;  
+    }
+    System.out.println("sum is : "+ sum);
+
     }
 }
