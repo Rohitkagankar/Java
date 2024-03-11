0dd026ae3cd55a54b95ab68c9c4218b47814674f
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-11 19:07:24 +0530

    reverse pyramid

--- Q_AdvPattern.java
+++ Q_AdvPattern.java
@@ -18,7 +18,21 @@ public class Q_AdvPattern {
             System.out.println();
         }
     }
+
+    public static void reverse_half_pyramid(int n){
+        for(int i=1; i<=n; i++){
+            //for outer loop
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         hollow_pattern(4,5);
+        reverse_half_pyramid(5);
     }
 }
