f8dbbc911b0c8358142754152c039ea89c44c71c
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-11 23:02:00 +0530

    butterfly

--- Q_AdvPattern.java
+++ Q_AdvPattern.java
@@ -31,8 +31,77 @@ public class Q_AdvPattern {
             System.out.println();
         }
     }
+
+    public static void numpyramid(int n){
+        for(int i=1; i<=n; i++){
+            //rows
+            int k=5;
+            for(int j=1; j<=(n-i+1); j++){
+                System.out.print(j);
+                
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void FloidsTri(int n){
+        int counter=1;
+        for(int i=1; i<=n; i++){
+            //rows
+            for(int j=1; j<=i; j++){
+                System.out.print(counter+" ");
+                counter++;
+            }
+            System.out.println();
+        }
+
+    }
+    public static void Trig01(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                if((i+j)%2==0){
+                    System.out.print("1");
+                }else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Butterfly(int n){
+        for (int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         hollow_pattern(4,5);
         reverse_half_pyramid(5);
+        numpyramid(5);
+        FloidsTri(5);
+        Trig01(5);
+        Butterfly(4);
     }
 }
