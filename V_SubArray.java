0f83cff1a4234b1b2597e59ad480119c8fe74240
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-13 18:32:05 +0530

    max sum of subarray

--- V_SubArray.java
+++ V_SubArray.java
@@ -4,12 +4,19 @@
 public class V_SubArray {
     public static void subArray(int array[]){
         int total=0;
+        int maxSum=Integer.MIN_VALUE;
+        int sum=0;
         for(int i=0; i<array.length; i++){
             //i
             for(int j=i; j<array.length; j++){
                 //j
+                sum=0;
                 for(int k=i; k<=j; k++){
-                    System.out.print(array[k]+" ");  
+                    System.out.print(array[k]+" ");
+                    sum=sum+array[k]; 
+                }
+                if(maxSum<sum){
+                    maxSum=sum;
                 }
                 total++;
                 System.out.println();
@@ -17,10 +24,11 @@ public class V_SubArray {
             System.out.println();
         }
         System.out.println("total pairs are : "+total);
+        System.out.println("max pair sum is : "+maxSum);
     }
 
     public static void main(String[] args) {
-        int array[]={1,2,3,4,5};
+        int array[]={2,4,6,8,10};
         subArray(array);
     }
 }
