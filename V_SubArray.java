a7130907d556a1c1180817709daf2e067f122e06
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-13 19:05:46 +0530

    using prefix method

--- V_SubArray.java
+++ V_SubArray.java
@@ -27,8 +27,36 @@ public class V_SubArray {
         System.out.println("max pair sum is : "+maxSum);
     }
 
+    //using prefix sum 
+    public static void PresubArray(int array[]){
+        int maxSum=Integer.MIN_VALUE;
+        int sum=0;
+        int prefix[]=new int[array.length];
+
+        prefix[0]=array[0];
+        for(int i=1; i<array.length; i++){
+            prefix[i]=prefix[i-1]+array[i];
+        }
+
+        for(int i=0; i<array.length; i++){
+            //i
+            for(int j=i; j<array.length; j++){
+                sum= i==0? prefix[j] : prefix[j]-prefix[i-1];
+                if(maxSum<sum){
+                    maxSum=sum;
+                }
+               
+            }
+            
+        }
+        System.out.println("max pair sum is : "+maxSum);
+    }
+
+
     public static void main(String[] args) {
         int array[]={2,4,6,8,10};
         subArray(array);
+        int arr[]={1,2,3,4,5};
+        PresubArray(arr);
     }
 }
