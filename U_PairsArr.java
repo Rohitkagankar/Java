671e1c702a4a852dc5f67eb57524a8dc74d117c9
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-13 17:40:48 +0530

    Pairs in array

---
+++ U_PairsArr.java
@@ -0,0 +1,21 @@
+public class U_PairsArr {
+
+    public static void pairs(int array[],int count){
+        for(int i=0; i<array.length; i++){
+            for(int j=i+1; j<array.length; j++){
+                System.out.print("("+array[i]+","+array[j]+")");
+                count++;
+            }
+        }
+        System.out.println();
+        System.out.println("Total no of pairs : "+count);
+
+    }
+    public static void main(String[] args) {
+        int array[]={2,4,6,8,10};
+        int count=0;
+        pairs(array,count);
+        // System.out.println(count);
+
+    }
+}
