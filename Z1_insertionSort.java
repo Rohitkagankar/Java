aed5b79105120527829dd197473b5b8d55d4fcf0
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-15 22:02:29 +0530

    insertion sort

---
+++ Z1_insertionSort.java
@@ -0,0 +1,27 @@
+public class Z1_insertionSort {
+    public static void insertionSort(int arr[]){
+        for(int i=1; i<arr.length; i++){
+            int curr=arr[i];
+            int pre=i-1;
+            while (pre>=0 && arr[pre]>curr) {
+                arr[pre+1]=arr[pre];
+                pre--;   
+            }
+            arr[pre+1]=curr;
+        }
+    }
+
+    public static void pArray(int array[]){
+        for(int i=0; i<array.length; i++){
+            System.out.print(array[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[]={5,4,3,2,1};
+        insertionSort(arr);
+        pArray(arr);
+
+    }
+}
--- Z_selectionSort.java
+++ Z_selectionSort.java
@@ -3,7 +3,7 @@ public class Z_selectionSort {
         for(int i=0; i<array.length-1; i++){
             int minPo=i;
             for(int j=i+1; j<array.length; j++){
-                if(array[i]>array[j]){
+                if(array[i]>array[j]){ //<for decresing order
                     minPo=j;
                 }
             }
