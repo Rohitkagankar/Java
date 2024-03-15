2a9353813a5b71f0a3b0a2aace31496ce14535f0
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-15 18:45:38 +0530

    for sorted array

--- Y_BubbleSort.java
+++ Y_BubbleSort.java
@@ -1,12 +1,18 @@
 public class Y_BubbleSort {
     public static void bubbleSort(int array[]){
         for(int i=0; i<array.length-1; i++){
+            int count=0;
             for(int j=0; j<array.length-1-i; j++){
                 if(array[j]>array[j+1]){
                     int temp=array[j];
                     array[j]=array[j+1];
                     array[j+1]=temp;
+                    count++;
                 }
+            }
+            if(count==0){
+                System.out.println("already sorted.");
+                break;
             }
         }
     }
@@ -18,7 +24,7 @@ public class Y_BubbleSort {
         System.out.println();
     }
     public static void main(String[] args) {
-        int array[]={2,4,6,3,8,5};
+        int array[]={2,3,4,5,6,7};
         bubbleSort(array);
         pArray(array);
     }
