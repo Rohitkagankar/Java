5891d4c4ca4384bd6a14f1d02da58767b92818c7
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-16 11:15:24 +0530

    search function

--- Z3_2dArray.java
+++ Z3_2dArray.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 
 public class Z3_2dArray {
+
+    public static boolean searchEle(int matrix[][],int key){
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j]==key){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int matrix[][]=new int[3][3];
@@ -18,5 +30,6 @@ public class Z3_2dArray {
             }
             System.out.println();
         }
+        System.out.println(searchEle(matrix, 6));
     }
 }
