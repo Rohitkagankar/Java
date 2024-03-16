0e523ef047b831328089179eaf8255dc81bf7b0a
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-16 11:09:23 +0530

    2d matrix

---
+++ Z3_2dArray.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Z3_2dArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int matrix[][]=new int[3][3];
+
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                matrix[i][j]=sc.nextInt();
+            }
+            // System.out.println();
+        }
+        
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
