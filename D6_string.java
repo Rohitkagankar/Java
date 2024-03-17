53094c88512a4e7498f8a30433a99e0a6a928009
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-17 15:52:14 +0530

    isPalindrone

--- D6_string.java
+++ D6_string.java
@@ -1,11 +1,22 @@
 import java.util.*;
 public class D6_string {
 
+    //print string function-----
     public static void printstring(String name){
         for(int i=0; i<name.length(); i++){
             System.out.print(name.charAt(i)+" ");
         }
         System.out.println();
+    }
+    //palindrone string---------
+    public static boolean isPalindrone(String a){
+        int n=a.length();
+        for(int i=0; i<a.length()/2; i++){
+            if(a.charAt(i)==a.charAt(n-i-1)){
+                return true;
+            }
+        }
+        return false;
     }
     public static void main(String[] args) {
         char ch[]={'a','b','c','d'};
@@ -24,5 +35,8 @@ public class D6_string {
         System.out.println(fulname);
         System.out.println(fulname.charAt(0));
         printstring(fulname);
+
+        String a="Abccba";
+        System.out.println(isPalindrone(a));
     }
 }
