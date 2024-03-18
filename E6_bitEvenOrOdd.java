452444a64bf18167c0b913936fd9e8a03c17d7e4
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-18 17:33:30 +0530

    bit at ith position

--- E6_bitEvenOrOdd.java
+++ E6_bitEvenOrOdd.java
@@ -7,8 +7,18 @@ public class E6_bitEvenOrOdd {
             System.out.println("Number is even.");
         }
     }
+    //get bit at i-th position----
+    public static int getIthBit(int num,int i){
+        int bit=1<<i;
+        if((num & bit)==0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
     public static void main(String[] args) {
         // int n=5;
         evenOdd(4);
+        System.out.println(getIthBit(10, 3));
     }
 }
