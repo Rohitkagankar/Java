44ca2f18a3e6530f912a4ffb0b230a49d0a70603
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-19 20:15:44 +0530

    access specifires

--- E7_oops1.java
+++ E7_oops1.java
@@ -9,8 +9,22 @@ public class E7_oops1 {
         Student s1=new Student();
         s1.name="rohit";
         System.out.println(s1.name);
+
+        BankDetails b1=new BankDetails();
+        b1.name="BOI";
+        // b1.password="asdf";  not allow 
+        b1.setPassword("abcd");
+        System.out.println(b1.name);
     }
 }
+class BankDetails{
+    public String name;
+    private String password;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
 class Pen{
     String color;
     int Pointer;
