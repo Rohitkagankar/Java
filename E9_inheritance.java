a4bc7668ae11a6d5495733baadd43b525c1cad7a
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-20 18:02:31 +0530

    inheritance

---
+++ E9_inheritance.java
@@ -0,0 +1,19 @@
+public class E9_inheritance {
+    public static void main(String[] args) {
+        Dog d1=new Dog();
+        d1.eat();
+    }
+}
+class Animal{
+    void eat(){
+        System.out.println("eats food.");
+    }
+    void breath(){
+        System.out.println("breath like human.");
+    }
+}
+class Dog extends Animal{
+    void bark(){
+        System.out.println("barking.");
+    }
+}
