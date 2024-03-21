3c9a23e71ad234d8f32eedd4579b543abaa08999
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-21 18:51:32 +0530

    static

---
+++ F8_static.java
@@ -0,0 +1,25 @@
+public class F8_static {
+    public static void main(String[] args) {
+        Student s1=new Student();
+        s1.schoolname="jspm";
+        Student s2=new Student();
+        System.out.println(s2.schoolname);
+    }
+}
+class Student{
+    String name;
+    int roll;
+
+    static int calPercentage(int phy,int math, int chem){
+        return (phy+chem+math)/3;
+    }
+
+    static String schoolname;
+
+    void setname( String name){
+        this.name=name;
+    }
+    String getname(){
+        return this.name;
+    }
+}
