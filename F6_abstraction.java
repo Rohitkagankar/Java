e9dc72cfdb48ade8e06da8f2f0dca4166326c8f1
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-21 18:02:03 +0530

    constructor

--- F6_abstraction.java
+++ F6_abstraction.java
@@ -4,9 +4,16 @@ public class F6_abstraction {
         c1.city();
         c1.mark();
         c1.name();
+        c1.changeColor();
+        System.out.println(c1.color);
+       
     }
 }
 abstract class Boy{
+    String color;
+    Boy(){
+        color="skin color";
+    }
     abstract void name();
     abstract void mark();
     void city(){
@@ -14,6 +21,10 @@ abstract class Boy{
     }
 }
 class Child extends Boy{
+    void changeColor(){
+        String color="black";
+        System.out.println(color);
+    }
     void name(){
         System.out.println("rohit");
     }
