d88432f0341a8e1519d8e1f97159854ccd5b6c14
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-22 22:27:54 +0530

    recorsion

--- G1_recursion.java
+++ G1_recursion.java
@@ -7,9 +7,21 @@ public class G1_recursion {
         System.out.print(n+" ");
         recorReverse(n-1);
     }
+    
+    public static void recInc(int n){
+        if(n==1){
+            System.out.print(n+" ");
+            return;
+        }
+        recInc(n-1);
+        System.out.print(n+" ");
+    }
     public static void main(String[] args) {
        int a=10;
        recorReverse(a);
+       System.out.println();
+       int b=15;
+       recInc(b);
     }
 }
 
