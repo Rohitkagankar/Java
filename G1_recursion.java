904b3adc12d1fd9430ed040a28d7b2f757d8099a
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-22 23:38:26 +0530

    factorial

--- G1_recursion.java
+++ G1_recursion.java
@@ -1,4 +1,6 @@
 public class G1_recursion {
+
+    //print 1 to n numbers in reverse order-----
     public static void recorReverse(int n){
         if(n==1){
             System.out.print(n);
@@ -7,7 +9,7 @@ public class G1_recursion {
         System.out.print(n+" ");
         recorReverse(n-1);
     }
-    
+    //print 1 to n numbers-----
     public static void recInc(int n){
         if(n==1){
             System.out.print(n+" ");
@@ -16,12 +18,27 @@ public class G1_recursion {
         recInc(n-1);
         System.out.print(n+" ");
     }
+    public static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        int fa=n * fact(n-1);
+        return fa;
+    }
     public static void main(String[] args) {
+       //print 1 to n numbers in reverse order-----
        int a=10;
        recorReverse(a);
        System.out.println();
+
+       //print 1 to n numbers-----
        int b=15;
        recInc(b);
+
+       System.out.println();
+       //print factorial of given number-----
+       int n=5;
+       System.out.println(fact(n));
     }
 }
 
