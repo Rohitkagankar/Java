06f09f56dbc58ac321bc70b50aaf27c298b57476
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-28 14:09:34 +0530

    substring

---
+++ G6_subString.java
@@ -0,0 +1,22 @@
+public class G6_subString {
+    public static void findSubsets(String str, String ans, int i){
+        //base case
+        if(i==str.length()){
+            if(ans.length()==0){
+                System.out.println("null");
+            }else{
+                System.out.println(ans);
+            }
+            return;
+        }
+        //recorsion step
+        //choice yes
+        findSubsets(str, ans+str.charAt(i), i+1);
+        //choice no
+        findSubsets(str, ans, i+1);
+    }
+    public static void main(String[] args) {
+        String str="abc";
+        findSubsets(str, "", 0);
+    }
+}
