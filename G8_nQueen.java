5a780a7dc494fb697ac6dc8dd175050f303effaf
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-29 10:14:50 +0530

    n queens problem

--- G8_nQueen.java
+++ G8_nQueen.java
@@ -1,4 +1,26 @@
 public class G8_nQueen {
+    public static boolean isSafe(char board[][],int row,int col){
+        //vertical up
+        for(int i=row-1; i>=0; i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+        //left diagonal
+        for(int i=row-1 ,j=col-1; i>=0 && j>=0; i-- ,j--){
+            if(board[i][j]=='Q'){
+                return false;
+            
+            }
+        }
+        //right diagonal
+        for(int i=row-1, j=col+1; i>=0 && j<board.length ; i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
     public static void nQueen(char board[][],int row){
         //base
         if(row==board.length){
@@ -7,21 +29,25 @@ public class G8_nQueen {
         }
         //recorsion
         for(int j=0; j<board.length; j++){
-            board[row][j]='Q';
-            nQueen(board, row+1); //function call
-            board[row][j]='x';
+            if(isSafe(board, row, j)){
+                board[row][j]='Q';
+                nQueen(board, row+1); //function call
+                board[row][j]='x';  
+            }
+            
         }
     }
     public static void printBoard(char board[][]){
+        System.out.println("---------chess board--------");
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board.length; j++){
-                System.out.print(board[i][j]);
+                System.out.print(board[i][j]+" ");
             }
             System.out.println();
         }
     }
     public static void main(String[] args) {
-        int n=2;
+        int n=4;
         char board [][]=new char[n][n];
         //initialize
         for(int i=0; i<board.length; i++){
