c8705b43610106b406e2ccacc84c9a06ce289136
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-04-02 07:59:27 +0530

    max, swap, asending ,desending

--- H2_ArrayList.java
+++ H2_ArrayList.java
@@ -1,6 +1,13 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class H2_ArrayList {
+    public static void swapArrList(ArrayList<Integer> li, int idx1, int idx2){
+        int temp= li.get(idx1);
+        li.set(idx1, li.get(idx2));
+        li.set(idx2, temp);
+
+
+    }
     public static void main(String[] args) {
         ArrayList<Integer> list1 =new ArrayList<>();
         ArrayList<Boolean> list2 =new ArrayList<>();
@@ -41,5 +48,50 @@ public class H2_ArrayList {
         }
         System.out.println();
         
+        //make arraylist and print in reverse order
+        ArrayList<Integer> li=new ArrayList<>();
+        li.add(1);
+        li.add(2);
+        li.add(3);
+        li.add(4);
+        li.add(5);
+        //print in reverse order
+        for(int i=li.size()-1; i>=0; i--){
+            System.out.print(li.get(i)+" ");
+        }
+        System.out.println();
+
+        //find maximum value O(n)
+        int max= Integer.MIN_VALUE;
+        for(int i=0; i<li.size(); i++){
+            // if(max<li.get(i)){
+            //     max=li.get(i);
+            // }
+            max=Math.max(max, li.get(i));
+        }
+        System.out.println(max);
+
+        //swap values 
+        int idx1=1;
+        int idx2=3;
+        System.out.println(li);
+        swapArrList(li, idx1, idx2);
+        System.out.println(li);
+        
+        //Arraylist sort elements
+        ArrayList<Integer> lista=new ArrayList<>();
+        lista.add(3);
+        lista.add(2);
+        lista.add(8);
+        lista.add(1);
+        lista.add(5);
+
+        //asending order
+        Collections.sort(lista);
+        System.out.println(lista);
+        
+        //desending order
+        Collections.sort(lista, Collections.reverseOrder());
+        System.out.println(lista);
     }
 }
