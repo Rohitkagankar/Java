32133c167336fcfdb48dbb849ab91747540e1a58
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-04-02 08:59:06 +0530

    multidimentional ArrayList

---
+++ H3_multiDimArrList.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class H3_multiDimArrList {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> mainList=new ArrayList<>();
+
+        ArrayList<Integer> list1= new ArrayList<>();
+        list1.add(1); list1.add(2);
+
+        ArrayList<Integer> list2= new ArrayList<>();
+        list2.add(3); list2.add(4);
+
+        mainList.add(list1);
+        mainList.add(list2);
+
+        //to print element of mainlist
+        for(int i=0; i<mainList.size(); i++){
+            ArrayList<Integer> currList= mainList.get(i);
+            for(int j=0; j<currList.size(); j++){
+                System.out.print(currList.get(j)+ " ");
+            }
+            System.out.println();
+        }
+        System.out.println(mainList);
+
+    }
+}
