57ecff0e60797a2100a6f050c5100b844c77a0ed
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-04-14 22:14:32 +0530

    iscycle

--- I1_isCycleLL.java
+++ I1_isCycleLL.java
@@ -29,7 +29,10 @@ public class I1_isCycleLL {
         head=new Node(1);
         head.next=new Node(2);
         head.next.next=new Node(3);
-        head.next.next.next=head;
+        head.next.next.next=new Node(4);
+        head.next.next.next.next=head;
         System.out.println(isCycle());
+
+        
     }
 }
