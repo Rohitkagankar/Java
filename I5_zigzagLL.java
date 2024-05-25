a416612231795b793b754d9cdd501a15041df4e4
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-05-25 22:19:12 +0530

    sum

---
+++ I5_zigzagLL.java
@@ -0,0 +1,10 @@
+public class I5_zigzagLL {
+    public static void sum(int a,int b){
+        System.out.println("sum is " + (a+b));
+    }
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        sum(10,20);
+
+    }
+}
