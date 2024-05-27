06f3f03185e401b0f1da54402ac2e1d14fa16659
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-05-27 23:21:48 +0530

    sub mul

--- I5_zigzagLL.java
+++ I5_zigzagLL.java
@@ -2,9 +2,17 @@ public class I5_zigzagLL {
     public static void sum(int a,int b){
         System.out.println("sum is " + (a+b));
     }
+    public static void sub(int a,int b){
+        System.out.println("sum is " + (a-b));
+    }
+    public static void mul(int a,int b){
+        System.out.println("sum is " + (a*b));
+    }
     public static void main(String[] args) {
         System.out.println("hello world");
         sum(10,20);
+        sub(10,20);
+        mul(10,20);
 
     }
 }
