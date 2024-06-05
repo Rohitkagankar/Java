72339919aea44c64abae58073dfacd15dc14da0e
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-06-05 22:56:21 +0530

    sroot

--- I5_zigzagLL.java
+++ I5_zigzagLL.java
@@ -14,6 +14,12 @@ public class I5_zigzagLL {
     public static void sqArea(int a, int b){
         System.out.println(a*b);
     }
+    public static void sqare(int a, int b){
+        System.out.println(a*b);
+    }
+    public static void sqareRoot(int a){
+        System.out.println(a*0.5);
+    }
 
     public static void main(String[] args) {
         System.out.println("hello world");
@@ -22,5 +28,7 @@ public class I5_zigzagLL {
         mul(10,20);
         div(20,10);
         sqArea(3,5);
+        sqare(2,5);
+        sqareRoot(0);
     }
 }
