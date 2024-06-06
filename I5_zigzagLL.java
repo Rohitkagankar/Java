0716eb5693b49ddd85fedcb8b2fff69bc0aa7283
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-06-06 13:22:54 +0530

    zigzag ll

--- I5_zigzagLL.java
+++ I5_zigzagLL.java
@@ -1,34 +1,101 @@
-public class I5_zigzagLL {
-    public static void sum(int a,int b){
-        System.out.println("sum is " + (a+b));
-    }
-    public static void sub(int a,int b){
-        System.out.println("sum is " + (a-b));
-    }
-    public static void mul(int a,int b){
-        System.out.println("sum is " + (a*b));
-    }
-    public static void div(int a,int b){
-        System.out.println("sum is " + (a/b));
-    }
-    public static void sqArea(int a, int b){
-        System.out.println(a*b);
-    }
-    public static void sqare(int a, int b){
-        System.out.println(a*b);
-    }
-    public static void sqareRoot(int a){
-        System.out.println(a*0.5);
-    }
+import org.w3c.dom.Node;
 
+public class I5_zigzagLL {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public static void addFirst(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+
+    }
+    public void addLast(int data){
+        //create node
+        Node newNode= new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        //tail->next = newnode
+        tail.next=newNode;
+        
+        //tail=newNode
+        tail=newNode;
+    }
+    public static void printList(){
+        if(head==null){
+            System.out.println("empty linked list.");
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"=>");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public void zigzag(){
+        //find the mid--
+        Node slow=head;
+        Node fast=head.next;
+        while (fast != null && fast.next!=null) {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        Node mid=slow;
+
+        //reverse the list--
+        Node curr=mid.next;
+        mid.next=null;
+        Node prev=null;
+        Node next;
+
+        while(curr != null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+
+        Node left=head;
+        Node right=prev;
+        Node nextL, nextR;
+
+        while (left!=null && right!=null) {
+            nextL=left.next;
+            left.next=right;
+            nextR=right.next;
+            right.next=nextL;
+
+            left=nextL;
+            right=nextR;
+            
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("hello world");
-        sum(10,20);
-        sub(10,20);
-        mul(10,20);
-        div(20,10);
-        sqArea(3,5);
-        sqare(2,5);
-        sqareRoot(0);
+       I5_zigzagLL l1=new I5_zigzagLL();
+       l1.addLast(5);
+       l1.addLast(4);
+       l1.addLast(3);
+       l1.addLast(2);
+       l1.addLast(1);
+       l1.printList();
+
+       l1.zigzag();
+       l1.printList();
     }
 }
