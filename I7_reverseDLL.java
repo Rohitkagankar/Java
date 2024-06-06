ce3ed75ac3056b0a3da92bce18570a4e5df393a4
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-06-06 22:24:55 +0530

    reverse DLL

--- I7_reverseDLL.java
+++ I7_reverseDLL.java
@@ -11,19 +11,59 @@ public class I7_reverseDLL {
         }
 
     }
-    public static addfirst(int data){
-        Node newnNode=new Node(data);
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public static void addfirst(int data){
+        Node newNode=new Node(data);
+        size++;
         if(head==null){
-            System.out.println("Empty Doubly ll");
+            head=tail=newNode;
             return;
         }
-        if(size==1){
-            int val=head.data;
-            head=head.next;
-            head.prev=null;
+        newNode.next=head;
+        head.prev=newNode;
+        head=newNode;
+        
+    }
+    public static void printDll(){
+        if(head==null){
+            System.out.println("Doubly ll is empty");
+            return;
         }
-        int val=head.data;
-        head=head.next;
-        head.prev=null;
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"<=>");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void reverse(){
+        Node curr=head;
+        Node prev=null;
+        Node next;
+        while (curr != null) {
+            next=curr.next;
+            curr.next=prev;
+            curr.prev=next;
+
+            prev=curr;
+            curr=next;
+            
+        }
+        head=prev;
+    }
+    public static void main(String[] args) {
+        I7_reverseDLL l1=new I7_reverseDLL();
+        l1.addfirst(5);
+        l1.addfirst(4);
+        l1.addfirst(3);
+        l1.addfirst(2);
+        l1.addfirst(1);
+        l1.printDll();
+        l1.reverse();
+        l1.printDll();
+        
     }
 }
