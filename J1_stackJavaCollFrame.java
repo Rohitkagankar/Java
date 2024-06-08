e54e7a220a9be0fa1e91b8d7df3107ab0a8157fb
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-06-08 07:45:50 +0530

    stack using java collection framework

---
+++ J1_stackJavaCollFrame.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class J1_stackJavaCollFrame {
+    public static void main(String[] args) {
+        Stack<Integer> s= new Stack<>();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        s.add(4);
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
