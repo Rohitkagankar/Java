82b90b449812d5fe5c5d1aba1fce9ae0cfc4048a
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-06-12 21:43:16 +0530

    is duplicate parenthesis

--- J6_validParenthesis.java
+++ J6_validParenthesis.java
@@ -25,9 +25,33 @@ public class J6_validParenthesis {
             return false;
         }
     }
+
+    public static boolean isDuplicate(String str){
+        Stack <Character> s=new Stack<>();
+
+        for(int i=0; i<str.length(); i++){
+            char curr=str.charAt(i);
+            
+            if(curr==')'){
+                int count=0;
+                while(s.pop() != '('){
+                    count++;
+                }
+                if(count<1){
+                    return true;
+                }
+            }else{
+                s.push(curr);
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         String str="[{()}]";
+        String str1="((a+b))";
         System.out.println(isValid(str));
+        System.out.println(isDuplicate(str1));
+
 
     }
 }
